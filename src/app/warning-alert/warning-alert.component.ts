c4e0499ff4be1968cded5ea53e6f11f599322bb7
Iraklis D <sithiraklis@yahoo.gr>
2024-05-01 09:30:50 +0100

    Add two way binding

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -5,16 +5,27 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './warning-alert.component.html',
   styleUrls: ['./warning-alert.component.css'],
 })
+
 export class WarningAlertComponent implements OnInit {
+
   allowNewServer = false;
   serverCreationStatus = 'No new server was created!';
+  serverName='FooServer';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 1000);
   }
+
   ngOnInit() {}
+
   onCreateServer() {
     this.serverCreationStatus = 'Server has been created!';
   }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
+
 }
