3de4ea89a89cec9a0944574a4aa628b8498f3090
Iraklis Diakos <sithiraklis@yahoo.gr>
2024-05-10 13:57:16 +0100

    Setup project layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import {WarningAlertComponent} from './warning-alert/warning-alert.component';
 import {FormsModule} from "@angular/forms";
 import {UsernameComponent} from './username/username.component';
 import { PasswordComponent } from './password/password.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,14 @@ import { PasswordComponent } from './password/password.component';
     SuccessAlertComponent,
     WarningAlertComponent,
     UsernameComponent,
-    PasswordComponent
+    PasswordComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
     imports: [
         BrowserModule,
